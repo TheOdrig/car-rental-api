6930d9b83b383f167b7c08751e19a1ea75bbfdb1
Mehmet Akif Uludag <akifuludag7@gmail.com>
2025-09-28 14:07:14 +0300

    Initial commit

---
+++ src/main/java/com/akif/starter/CarGalleryProjectApplication.java
@@ -0,0 +1,13 @@
+package com.akif.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarGalleryProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarGalleryProjectApplication.class, args);
+    }
+
+}
